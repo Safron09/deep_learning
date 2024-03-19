88fca2bc42ba9373e625f9388a85ff0c5ea551b3
Safron09 <y.safronnikov@gmail.com>
2024-03-19 17:47:13 -0400

    Layers, Compile, Fit

--- LSTM_google_stock_price/LSTM_Google_stock_price_trend.py
+++ LSTM_google_stock_price/LSTM_Google_stock_price_trend.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# PART 1
 # Data Import
 dataset_train = pd.read_csv("Data_sets/google_stock_prices/Google_Stock_Price_Train.csv")
 training_set = dataset_train.iloc[:, 1:2].values    # Upper Bound excluded
@@ -12,5 +13,48 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range=(0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
+# Create a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
 
-print(training_set_scaled)
+# Reshaping from 2 to 3 dimensions. 1 outside [] - indicator
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+# PART 2
+# Building LSTM
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Initialize the RNN
+regressor = Sequential()
+
+# Adding Layers
+# 1 Layer
+regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+regressor.add(Dropout(rate=0.2))
+# 2 Layer
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(rate=0.2))
+# 3 Layer
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(rate=0.2))
+# 4 Layer
+regressor.add(LSTM(units=50))
+regressor.add(Dropout(rate=0.2))
+# Output
+regressor.add(Dense(units=1))
+
+# Compiling the RNN
+regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+# Fitting the RNN to the training set
+regressor.fit(X_train, y_train, epochs=100, batch_size=32)
+
+
+print(regressor)
