49f67414cfbf20372e31860e9312deed15940f3c
Safron09 <y.safronnikov@gmail.com>
2024-03-23 12:35:42 -0400

    working model with prediction in HTML

--- LSTM_google_stock_price/LSTM_Google_stock_price_trend.py
+++ LSTM_google_stock_price/LSTM_Google_stock_price_trend.py
@@ -1,6 +1,10 @@
 import numpy as np
+import matplotlib
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.graph_objects as go
+import plotly.io as pio
 
 # PART 1
 # Data Import
@@ -64,11 +68,46 @@ real_stock_price = dataset_test.iloc[:, 1:2].values
 # Predicting Stock Price
 dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0)
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
-inputs = inputs.reshape(-1,1)
+inputs = inputs.reshape(-1, 1)
 inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
+# Visualising
+# plt.plot(real_stock_price, color='red', label='Real Google Stock Price')
+# plt.plot(predicted_stock_price, color='blue', label='Predicted Google Stock Price')
+# plt.title('Google Stock Price Prediction')
+# plt.xlabel('Time')
+# plt.ylabel('Stock Price')
+# plt.legend()
+# plt.show()
 
+trace1 = go.Scatter(
+    x = list(range(len(real_stock_price))),
+    y = real_stock_price.flatten(),  # Assuming real_stock_price is a numpy array
+    mode = 'lines',
+    name = 'Real Stock Price'
+)
+trace2 = go.Scatter(
+    x = list(range(len(predicted_stock_price))),
+    y = predicted_stock_price.flatten(),  # Assuming predicted_stock_price is a numpy array
+    mode = 'lines',
+    name = 'Predicted Stock Price'
+)
+# Layout
+layout = go.Layout(
+    title='Google Stock Price Prediction',
+    xaxis=dict(title='Time'),
+    yaxis=dict(title='Stock Price')
+)
+# Figure
+fig = go.Figure(data=[trace1, trace2], layout=layout)
+# Show figure in a browser
+fig.write_html('temp-plot.html', auto_open=True)
 
-
-
-print(real_stock_price)
+print(predicted_stock_price)
