efeb3facf6526f047f5e72b66e4b7fe6a1298fd8
Safron09 <y.safronnikov@gmail.com>
2024-03-26 18:40:25 -0400

    New

--- LSTM_google_stock_price/LSTM_Google_stock_price_trend.py
+++ LSTM_google_stock_price/LSTM_Google_stock_price_trend.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
+from keras.callbacks import EarlyStopping
+
 
 # PART 1
 # Data Import
-dataset_train = pd.read_csv("Data_sets/google_stock_prices/Google_Stock_Price_Train.csv")
+dataset_train = pd.read_csv("Data_sets/KO/KO_feb_24_2004_to_mar_23_2004.csv")
 training_set = dataset_train.iloc[:, 1:2].values    # Upper Bound excluded
 
 # Feature Scaling
@@ -21,7 +23,7 @@ training_set_scaled = sc.fit_transform(training_set)
 X_train = []
 y_train = []
 # To update correct ranges of the test file strings amount use -1 from the list
-for i in range(60, 1258):
+for i in range(60, 5035):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
@@ -59,11 +61,17 @@ regressor.add(Dense(units=1))
 regressor.compile(optimizer='adam', loss='mean_squared_error')
 
 # Fitting the RNN to the training set
-regressor.fit(X_train, y_train, epochs=100, batch_size=32)
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    patience=10,         # Number of epochs with no improvement
+    verbose=1,
+    restore_best_weights=True  # Restores model weights from the epoch with the best value of the monitored metric
+)
+regressor.fit(X_train, y_train, epochs=100, batch_size=32, callbacks=[early_stopping])
 
 # Part 3
 # Setting real stock price
-dataset_test = pd.read_csv("Data_sets/google_stock_prices/Google_Stock_Price_Test.csv")
+dataset_test = pd.read_csv("Data_sets/KO/KO_feb_23_24_to_mar_23_24.csv")
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Predicting Stock Price
