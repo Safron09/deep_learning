3e36506098736217f149d3200720264e703caf23
Safron09 <y.safronnikov@gmail.com>
2024-05-06 18:11:29 -0400

    Boltzman step 1

---
+++ boltzman/blotzman_recommendationSystem.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+# Dataset Import
+movies = pd.read_csv('Data_sets/Boltzman/ml-1m/movies.dat', sep='::', header=None, engine='python',
+                     encoding='latin-1')
+users = pd.read_csv('Data_sets/Boltzman/ml-1m/users.dat', sep='::', header=None, engine='python',
+                     encoding='latin-1')
+ratings = pd.read_csv('Data_sets/Boltzman/ml-1m/ratings.dat', sep='::', header=None, engine='python',
+                     encoding='latin-1')
