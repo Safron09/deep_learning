c8036d5cac9ee6abf1a72432f119c270e1c81266
Safron09 <y.safronnikov@gmail.com>
2024-05-08 17:53:36 -0400

    step3

--- boltzman/blotzman_recommendationSystem.py
+++ boltzman/blotzman_recommendationSystem.py
@@ -21,3 +21,10 @@ training_set = np.array(training_set, dtype='int')
 
 test_set = pd.read_csv('Data_sets/Boltzman/ml-100k/u1.test', delimiter='\t')
 test_set = np.array(test_set, dtype='int')
+
+# Number of users
+nb_users = int(max(max(training_set[:, 0]), max(test_set[:, 0])))
+nb_movies = int(max(max(training_set[:, 1]), max(test_set[:, 1])))
+
+print(nb_users)
+print(nb_movies)
