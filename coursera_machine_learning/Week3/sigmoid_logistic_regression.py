06144a8d5185221fe06dced51603eaaba1c754a3
Yuriy Safronnynov <ysafronnynov@fortressinfosec.com>
2024-08-07 16:18:43 -0400

    decision_boundary

--- coursera_machine_learning/Week3/sigmoid_logistic_regression.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from plt_one_addpt_onclick import plt_one_addpt_onclick
-from lab_utils_common import draw_vthresh
-
-plt.style.use('ML_and_DL/coursera_machine_learning/Week3/deeplearning.mplstyle.md')
-
