de2ac7ee2cbffa357fa326663a19dd8d6783d476
Yuriy Safronnynov <ysafronnynov@fortressinfosec.com>
2024-08-12 15:48:05 -0400

    cost function

---
+++ coursera_machine_learning/Week3/gradient_descent_for_logistic_regression.py
@@ -0,0 +1,11 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_common import  dlc, plot_data, plt_tumor_data, sigmoid, compute_cost_logistic
+from plt_quad_logistic import plt_quad_logistic, plt_prob
+
+
+
+plt.style.use('ML_and_DL/coursera_machine_learning/Week3/deeplearning.mplstyle.md')
+
+
